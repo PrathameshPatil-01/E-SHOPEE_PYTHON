32de75744325e1342e59eeb2768e84c7c7033a7a
PrathameshPatil-01 <prathameshp800@gmail.com>
2021-11-29 11:41:29 +0530

    add product with type

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for,request
+from flask import Flask, render_template,url_for,request,redirect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,SelectField,PasswordField,validators
@@ -19,6 +19,7 @@ db = SQLAlchemy(app)
 ##creating table
 class Cards(db.Model):
     id=db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(200), nullable=False)
     title=db.Column(db.String(200), nullable=False)
     img_url=db.Column(db.String, nullable=False)
     specs=db.Column(db.String, nullable=False)
@@ -51,6 +52,7 @@ class Reset(FlaskForm):
     submit = SubmitField(label="Send Verification Code")
 
 class Products(FlaskForm):
+    type = StringField(label='Classify Product', validators=[DataRequired()])
     title=StringField(label='Title', validators=[DataRequired()])
     img_url=StringField(label='Img_url', validators=[DataRequired()])
     specs=StringField(label='Specifications', validators=[DataRequired()])
@@ -62,7 +64,7 @@ class Products(FlaskForm):
 def login():
     login_form = Login()
     if login_form.validate_on_submit():
-        return render_template("home.html")
+        return redirect(url_for('home'))
 
     return render_template("login.html", form=login_form)
 
@@ -70,7 +72,7 @@ def login():
 def signup():
     signup_form= Signup()
     if signup_form.validate_on_submit():
-        return render_template("home.html")
+        return render_template("login.html")
     return render_template("signup.html", form=signup_form)
 
 
@@ -82,42 +84,58 @@ def reset():
 
     return render_template("reset.html",form=form)
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
-@app.route("/mobiles")
-def mobiles():
-    return render_template("mobiles.html")
-
-@app.route("/electronics")
-def electronics():
-    return render_template("electronics.html")
-
-@app.route("/space_store")
-def space_store():
-    return render_template("space_store.html")
-
-@app.route("/makeup")
-def makeup():
-    return render_template("makeup.html")
-
-@app.route("/kitchen")
-def kitchen():
-    return render_template("kitchen.html")
-
-@app.route("/computer")
-def computer():
-    return render_template("computer.html")
-
-
 @app.route("/add_card",methods=["GET", "POST"])
 def add_card():
     form = Products()
     if form.validate_on_submit():
+        new_card = Cards(
+            type=form.type.data.lower(),
+            title=form.title.data,
+            img_url=form.img_url.data,
+            specs=form.specs.data,
+            price=int(form.price.data),
+            offer=int(form.offer.data)
+        )
+        db.session.add(new_card)
+        db.session.commit()
         return render_template("home.html")
-
     return render_template("add_card.html",form=form)
+
+
+@app.route("/home")
+def home():
+    cards = Cards.query.all()
+    return render_template("home.html", cards=cards)
+
+@app.route("/mobiles")
+def mobiles():
+    cards=Cards.query.filter_by(type="mobiles")
+    return render_template("mobiles.html", cards=cards)
+
+@app.route("/electronics")
+def electronics():
+    cards = Cards.query.filter_by(type="electronics")
+    return render_template("electronics.html", cards=cards)
+
+@app.route("/space_store")
+def space_store():
+    cards = Cards.query.filter_by(type="space store")
+    return render_template("space_store.html", cards=cards)
+
+@app.route("/makeup")
+def makeup():
+    cards = Cards.query.filter_by(type="beauty and makeup")
+    return render_template("makeup.html", cards=cards)
+
+@app.route("/kitchen")
+def kitchen():
+    cards = Cards.query.filter_by(type="home and kitchen")
+    return render_template("kitchen.html", cards=cards)
+
+@app.route("/computer")
+def computer():
+    cards = Cards.query.filter_by(type="computer")
+    return render_template("computer.html", cards=cards)
 
 
 if __name__ == '__main__':
