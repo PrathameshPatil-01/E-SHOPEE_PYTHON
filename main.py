751d9334fcb1d9971ab8d2baeb0002a517f8f0a9
PrathameshPatil-01 <prathameshp800@gmail.com>
2021-12-01 23:38:55 +0530

    postgres

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-
+import os
 
 
 app=Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 ##CREATING DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cards.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
