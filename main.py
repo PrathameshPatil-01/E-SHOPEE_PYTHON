7d980301874015e593eff130041c36c20aea3181
PrathameshPatil-01 <prathameshp800@gmail.com>
2021-12-01 23:43:05 +0530

    postgres

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 ##CREATING DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///cards.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
